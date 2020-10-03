7c00673ffba73a2a02b5a695faa11c473e746b20
kelbri10 <kelbri10@gmail.com>
2020-10-03 01:22:36 -0400

    created cards for portfolio.html display projects and descriptions

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//view and github buttons portfolio.html 
+const viewBtn = document.getElementsByClassName('view-btn'); 
+const githubBtn = document.getElementsByClassName('github-btn'); 
+
+viewBtn.addEventListener('click', () => { 
+
+}); 
+
+githubBtn.addEventListener('click', () => { 
+
+}); 
+
+//submit button for contact.html 
 const submitBtn = document.getElementById('submit-btn'); 
 
 submitBtn.addEventListener('click', () => { 
